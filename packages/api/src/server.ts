6e202c10a1b5651013878378e5a53964754a48a2
maceip <ryan.macarthur@gmail.com>
2024-03-30 04:13:33 -0400

    cors

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -10,7 +10,8 @@ const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
     origin: [
-			'http://localhost:5173',
+			'http://localhost:3000',
+			'https://illusionblaze.com',
 			'https://nameless-terror-client.fly.dev',
 			'https://nameless-terror-api.fly.dev',
 		],
